8ae99c512c4758671a01e8279a22d5b1e9640806
muhaiminjobayer <mmj0993@gmail.com>
2024-12-20 02:06:12 -0600

    Add CategoryService interface with methods for category operations
    - Added `CategoryService` interface with `getAllCategories` and `createCategory` methods

---
+++ src/main/java/com/ecommerce/sb_ecom/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.ecommerce.sb_ecom.service;
+
+import com.ecommerce.sb_ecom.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+}
