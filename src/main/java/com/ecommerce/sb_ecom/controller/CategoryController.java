fd34d5ceee21142d5b6f8d72f2af06e8c527407a
muhaiminjobayer <mmj0993@gmail.com>
2024-12-21 02:07:31 -0600

    Add deleteCategory endpoint to CategoryController

--- src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -3,10 +3,7 @@ package com.ecommerce.sb_ecom.controller;
 
 import com.ecommerce.sb_ecom.model.Category;
 import com.ecommerce.sb_ecom.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +24,15 @@ public class CategoryController {
 
     @PostMapping("api/public/categories")
     public String createCategory(@RequestBody  Category category) {
+        category.setCategoryId((long) (categoryService.getAllCategories().size() + 1));
         categoryService.createCategory(category);
         return "Category created successfully";
+    }
+
+    @DeleteMapping("api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 
 }
