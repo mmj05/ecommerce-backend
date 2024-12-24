57df25640be1da898f9ed43a5186750d25c1f4ee
muhaiminjobayer <mmj0993@gmail.com>
2024-12-24 09:07:03 -0600

    Utilize JPARepository for streamlined method updates in CategoryServiceImpl

--- src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecommerce.sb_ecom.service;
 
 import com.ecommerce.sb_ecom.model.Category;
+import com.ecommerce.sb_ecom.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,38 +14,43 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private List<Category> catagories = new ArrayList<>();
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return catagories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        catagories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        List<Category> catagories = categoryRepository.findAll();
         Category  category = catagories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId)).findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
-        catagories.remove(category);
+        categoryRepository.delete(category);
 
         return "Category with categoryId: " + categoryId + " deleted successfully";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+        List<Category> catagories = categoryRepository.findAll();
+
         Optional<Category> optionalCategory = catagories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
 
         if (optionalCategory.isPresent()) {
             Category existingCategory = optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
+            Category savedCategory = categoryRepository.save(existingCategory);
+            return savedCategory;
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found");
         }
