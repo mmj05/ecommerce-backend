5021d53f7d29e53229c5c2b2d453d35da5a57078
muhaiminjobayer <mmj0993@gmail.com>
2024-12-24 09:04:16 -0600

    Update the Category entity to use JPA

--- src/main/java/com/ecommerce/sb_ecom/model/Category.java
+++ src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -1,15 +1,18 @@
 package com.ecommerce.sb_ecom.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "category")
 public class Category {
 
     private Long categoryId;
     private String categoryName;
 
-    public Category(String categoryName, Long categoryId) {
-        this.categoryName = categoryName;
-        this.categoryId = categoryId;
-    }
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getCategoryId() {
         return categoryId;
     }
@@ -17,6 +20,12 @@ public class Category {
     public String getCategoryName() {
         return categoryName;
     }
+
+    public Category(String categoryName, Long categoryId) {
+        this.categoryName = categoryName;
+        this.categoryId = categoryId;
+    }
+
 
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
