1f75b53c20326b633e0a2887cb1bcb30e0052300
muhaiminjobayer <mmj0993@gmail.com>
2024-12-24 09:05:10 -0600

    Update to use JPA

--- src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
@@ -1,4 +1,8 @@
 package com.ecommerce.sb_ecom.repositories;
 
-public interface CategoryRepository {
+import com.ecommerce.sb_ecom.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 }
