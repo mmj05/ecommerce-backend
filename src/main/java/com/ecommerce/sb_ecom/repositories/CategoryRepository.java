f65d3ba0babe96f87dbb96009acce37686186d7c
muhaiminjobayer <mmj0993@gmail.com>
2024-12-30 11:41:59 -0600

    Extend CategoryRepository with custom query method to find category by name.

--- src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
@@ -1,8 +1,10 @@
 package com.ecommerce.sb_ecom.repositories;
 
 import com.ecommerce.sb_ecom.model.Category;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    Category findByCategoryName(String categoryName);
 }
