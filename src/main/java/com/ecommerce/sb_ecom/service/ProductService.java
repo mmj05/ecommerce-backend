5c4a6641edb173626a14c733adaa5aae1784d6df
muhaiminjobayer <mmj0993@gmail.com>
2025-01-07 11:25:42 -0600

    Update ProductService interface to include methods for pagination, sorting, product image updates, and additional CRUD operations

--- src/main/java/com/ecommerce/sb_ecom/service/ProductService.java
+++ src/main/java/com/ecommerce/sb_ecom/service/ProductService.java
@@ -1,15 +1,23 @@
 package com.ecommerce.sb_ecom.service;
 
-import com.ecommerce.sb_ecom.model.Product;
 import com.ecommerce.sb_ecom.payload.ProductDTO;
 import com.ecommerce.sb_ecom.payload.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface ProductService {
-    ProductDTO addProduct(Product product, Long categoryId);
+    ProductDTO addProduct(ProductDTO productDTO, Long categoryId);
 
-    ProductResponse getAllProducts();
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
-    ProductResponse getProductsByCategory(Long categoryId);
+    ProductResponse getProductsByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
-    ProductResponse getProductsByKeyWord(String keyWord);
+    ProductResponse getProductsByKeyWord(String keyWord, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+
+    ProductDTO deleteProduct(Long productId);
+
+    ProductDTO updateProduct(ProductDTO productDTO, Long productId);
+
+    ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
 }
