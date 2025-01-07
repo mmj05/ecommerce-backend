bdc90f8550497e94bfbbae09d41bbe736a46d581
muhaiminjobayer <mmj0993@gmail.com>
2025-01-07 11:26:49 -0600

    Update ProductRepository to support pagination for category-based and keyword-based product retrieval, and add a method to check product existence by name

--- src/main/java/com/ecommerce/sb_ecom/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repositories/ProductRepository.java
@@ -2,13 +2,15 @@ package com.ecommerce.sb_ecom.repositories;
 
 import com.ecommerce.sb_ecom.model.Category;
 import com.ecommerce.sb_ecom.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategoryOrderByPriceAsc(Category category);
-    List<Product> findByProductNameLikeIgnoreCase(String keyword);
+    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageable);
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageable);
+
+    boolean existsByProductName(String productName);
 }
