7a7d462c97dc41747ba97e51da800298165e4881
muhaiminjobayer <mmj0993@gmail.com>
2025-01-07 11:28:43 -0600

    Add validation annotations to Product entity for productName and description; enforce minimum character length and non-null constraints.

--- src/main/java/com/ecommerce/sb_ecom/model/Product.java
+++ src/main/java/com/ecommerce/sb_ecom/model/Product.java
@@ -1,6 +1,8 @@
 package com.ecommerce.sb_ecom.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +16,14 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name should have atleast 3 characters")
     private String productName;
     private String image;
+
+    @NotBlank
+    @Size(min = 5, message = "Description should have atleast 5 characters")
     private String description;
     private Integer quantity;
     private double price;
