c7d56924b4ae871d1ac795c091a15c344c5b30cc
muhaiminjobayer <mmj0993@gmail.com>
2025-02-17 11:09:12 -0600

    Add method to find cart by user email in CartRepository

---
+++ src/main/java/com/ecommerce/sb_ecom/repositories/CartRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.sb_ecom.repositories;
+
+import com.ecommerce.sb_ecom.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
+    Cart findCartByEmail(String email);
+}
