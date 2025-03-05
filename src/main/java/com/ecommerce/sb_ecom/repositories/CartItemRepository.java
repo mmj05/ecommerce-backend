a3f395444d9338020771011b82269d91e0e98b59
muhaiminjobayer <mmj0993@gmail.com>
2025-03-05 12:34:33 -0600

    Add @Modifying and @Transactional annotations for cart item deletion method.

--- src/main/java/com/ecommerce/sb_ecom/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repositories/CartItemRepository.java
@@ -2,10 +2,17 @@ package com.ecommerce.sb_ecom.repositories;
 
 import com.ecommerce.sb_ecom.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     @Query("SELECT ci FROM CartItem ci WHERE ci.cart.cartId = ?1 AND ci.product.productId = ?2")
     CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM CartItem ci WHERE ci.cart.cartId = ?1 AND ci.product.productId = ?2")
+    void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
