0b86886374886cbcd865810b6c4646c6b8663877
muhaiminjobayer <mmj0993@gmail.com>
2025-03-13 04:09:18 -0500

    Refactor User entity to update address relationship and improve mappings

--- src/main/java/com/ecommerce/sb_ecom/model/User.java
+++ src/main/java/com/ecommerce/sb_ecom/model/User.java
@@ -57,10 +57,7 @@ public class User {
 
     @Getter
     @Setter
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinTable(name = "user_address",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "address_id"))
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<Address> addresses = new ArrayList<>();
 
     @ToString.Exclude
