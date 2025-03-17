c637ccb84c4a748ca1841382522b634837031c10
muhaiminjobayer <mmj0993@gmail.com>
2025-03-17 14:45:50 -0500

    Refactor AddressRepository to retrieve addresses associated with a user by userId.

--- src/main/java/com/ecommerce/sb_ecom/repositories/AddressRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repositories/AddressRepository.java
@@ -2,9 +2,11 @@ package com.ecommerce.sb_ecom.repositories;
 
 import com.ecommerce.sb_ecom.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
     List<Address> findByUserUserId(Long userId);
 }
