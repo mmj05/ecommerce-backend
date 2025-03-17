4f44128502ee708a39f65e1ec3f2f08e51cb8cba
muhaiminjobayer <mmj0993@gmail.com>
2025-03-17 14:33:53 -0500

    Add @Repository annotation to UserRepository

--- src/main/java/com/ecommerce/sb_ecom/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.ecommerce.sb_ecom.repositories;
 
 import com.ecommerce.sb_ecom.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String userName);
