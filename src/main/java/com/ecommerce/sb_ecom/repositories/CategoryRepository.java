be5b6af2f96e7c056316c7050859aa20a4bc0efd
muhaiminjobayer <mmj0993@gmail.com>
2025-03-17 14:42:57 -0500

    Add @Repository annotation for CategoryRepository and ensure findByCategoryName functionality

--- src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.ecommerce.sb_ecom.repositories;
 import com.ecommerce.sb_ecom.model.Category;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findByCategoryName(String categoryName);
