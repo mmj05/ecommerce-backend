a8553e36c09b64826b5da007e36db4faba359982
muhaiminjobayer <mmj0993@gmail.com>
2025-03-23 14:09:58 -0500

    Rename class

--- src/main/java/com/ecommerce/ecom/EcomApplication.java
+++ src/main/java/com/ecommerce/ecom/EcomApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbEcomApplication {
+public class EcomApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbEcomApplication.class, args);
+		SpringApplication.run(EcomApplication.class, args);
 	}
 
 }
