335a0340ca3a512404f401470179349dbb94f389
muhaiminjobayer <mmj0993@gmail.com>
2025-07-06 01:01:35 -0500

    Remove dotenv loading logic from EcomApplication to simplify application startup and eliminate unnecessary dependencies.

--- src/main/java/com/ecommerce/ecom/EcomApplication.java
+++ src/main/java/com/ecommerce/ecom/EcomApplication.java
@@ -1,6 +1,5 @@
 package com.ecommerce.ecom;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,16 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcomApplication {
 
 	public static void main(String[] args) {
-		// Load .env file
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing() // Don't fail if .env file is missing (useful for production)
-				.load();
-		
-		// Set system properties from .env file
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
-		
 		SpringApplication.run(EcomApplication.class, args);
 	}
 
